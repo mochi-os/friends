c0d82244b4921d8d508193c120038f9929917f31
Alistair Cunningham <acunningham@integrics.com>
2025-12-20 13:57:45 +0900

    Web updates

--- web/src/hooks/usePageTitle.ts
+++ web/src/hooks/usePageTitle.ts
@@ -1,15 +1,7 @@
 import { useEffect } from 'react'
-import { useLayout } from '@mochi/common'
 
 export function usePageTitle(title: string) {
-  const { setTopBarTitle } = useLayout()
-
   useEffect(() => {
     document.title = `${title} - Mochi`
-    setTopBarTitle(title)
-
-    return () => {
-      setTopBarTitle(null)
-    }
-  }, [title, setTopBarTitle])
+  }, [title])
 }
